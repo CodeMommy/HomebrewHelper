5863291751e654247c4a4c316a3a61701b8020ed
Candison <kandisheng@163.com>
2021-01-02 00:18:20 +0800

    Update mirror to Tsinghua University

--- README.md
+++ README.md
@@ -13,7 +13,7 @@ Visit the [Homebrew homepage](https://brew.sh) to get more information.
 
 ### Install
 
-> With changing repositories to the mirror of Tencent Cloud
+> With changing repositories to the mirror of Tsinghua University
 
 ```shell script
 /bin/bash -c "$(curl -fsSL https://cdn.jsdelivr.net/gh/CodeMommy/HomebrewHelper@main/shell/install.sh)"
@@ -25,7 +25,7 @@ Visit the [Homebrew homepage](https://brew.sh) to get more information.
 /bin/bash -c "$(curl -fsSL https://cdn.jsdelivr.net/gh/CodeMommy/HomebrewHelper@main/shell/uninstall.sh)"
 ```
 
-### Change repositories to the mirror of Tencent Cloud
+### Change repositories to the mirror of Tsinghua University
 
 ```shell script
 /bin/bash -c "$(curl -fsSL https://cdn.jsdelivr.net/gh/CodeMommy/HomebrewHelper@main/shell/mirror.sh)"
@@ -42,4 +42,5 @@ Visit the [Homebrew homepage](https://brew.sh) to get more information.
 * [GitHub](https://github.com)
 * [Homebrew](https://brew.sh)
 * [Tencent Cloud](https://mirrors.cloud.tencent.com)
+* [Tsinghua University](https://mirrors.tuna.tsinghua.edu.cn)
 * [jsDelivr](https://www.jsdelivr.com)
--- shell/mirror.sh
+++ shell/mirror.sh
@@ -3,13 +3,15 @@
 set -e
 set -u
 
-BREW_REPOSITORY_HOST=https://mirrors.cloud.tencent.com/homebrew
+MIRROR_NAME="Tsinghua University"
+BREW_REPOSITORY_HOST=https://mirrors.tuna.tsinghua.edu.cn/git/homebrew
+BREW_BOTTLE_HOST=https://mirrors.tuna.tsinghua.edu.cn/homebrew-bottles
 REPOSITORIES=(core cask cask-fonts cask-drivers python science)
 SHELL_CONFIGURATION_FILES=(~/.bash_profile ~/.zshrc)
 
 brew_folder=$(brew --repo)
 if [ -d "$brew_folder" ]; then
-    echo "HomebrewHelper: Changing repository $brew_folder to the mirror of Tencent Cloud."
+    echo "HomebrewHelper: Changing repository $brew_folder to the mirror of $MIRROR_NAME."
     git -C "$brew_folder" remote set-url origin "$BREW_REPOSITORY_HOST/brew.git"
 else
     echo "HomebrewHelper: Repository $brew_folder is not exist."
@@ -18,7 +20,7 @@ fi
 for repository in ${REPOSITORIES[*]}; do
     folder=$(brew --repo "homebrew/$repository")
     if [ -d "$folder" ]; then
-        echo "HomebrewHelper: Changing repository $folder to the mirror of Tencent Cloud."
+        echo "HomebrewHelper: Changing repository $folder to the mirror of $MIRROR_NAME."
         git -C "$folder" remote set-url origin "$BREW_REPOSITORY_HOST/homebrew-$repository.git"
     else
         echo "HomebrewHelper: Repository $folder is not exist."
@@ -26,10 +28,10 @@ for repository in ${REPOSITORIES[*]}; do
 done
 
 for shell_configuration_file in ${SHELL_CONFIGURATION_FILES[*]}; do
-    echo "HomebrewHelper: Updating $shell_configuration_file with changing HOMEBREW_BOTTLE_DOMAIN to the mirror of Tencent Cloud."
+    echo "HomebrewHelper: Updating $shell_configuration_file with changing HOMEBREW_BOTTLE_DOMAIN to the mirror of $MIRROR_NAME."
     touch "$shell_configuration_file"
     sed -ie '/export HOMEBREW_BOTTLE_DOMAIN=/d' "$shell_configuration_file"
-    echo 'export HOMEBREW_BOTTLE_DOMAIN=https://mirrors.cloud.tencent.com/homebrew-bottles' >>"$shell_configuration_file"
+    echo "export HOMEBREW_BOTTLE_DOMAIN=$BREW_BOTTLE_HOST" >>"$shell_configuration_file"
     # shellcheck disable=SC1090
     source "$shell_configuration_file"
 done
